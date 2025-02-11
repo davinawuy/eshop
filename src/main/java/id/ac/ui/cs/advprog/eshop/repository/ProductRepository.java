db13d6aa648e648c57ef29e5dc8a85c284ca19c0
davinawuy <alano.davin@office.ui.ac.id>
2025-02-11 11:14:50 +0700

    Updating ProductController.java and ProductRepository.java to include a findid and edit function.

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -30,5 +30,24 @@ public class ProductRepository {
             }
         }
     }
+
+    public Product findid(int productId) {
+        for (Product product : productData) {
+            if (product.getProductId() == productId) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Product update(Product updatedProduct) {
+        for (int i = 0; i < productData.size(); i++) {
+            if (productData.get(i).getProductId() == updatedProduct.getProductId()) {
+                productData.set(i, updatedProduct);
+                return updatedProduct;
+            }
+        }
+        return null;
+    }
 }
 
