e13877bcb915cf49c14ab4a2d61bd37c345b6bd4
davinawuy <alano.davin@office.ui.ac.id>
2025-02-25 16:54:25 +0700

    Creating and Implementing a ProductRepositoryInterface to achieve DIP

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -2,28 +2,31 @@ package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 @Repository
-public class ProductRepository {
+public class ProductRepository implements ProductRepositoryInterface {
     private List<Product> productData = new ArrayList<>();
 
+    @Override
     public Product create(Product product) {
         productData.add(product);
         return product;
     }
 
+    @Override
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
 
+    @Override
     public void delete(String productId) {
         productData.removeIf(product -> product.getProductId().equals(productId));
     }
 
+    @Override
     public Product findid(String productId) {
         for (Product product : productData) {
             if (product.getProductId().equals(productId)) {
@@ -33,6 +36,7 @@ public class ProductRepository {
         return null;
     }
 
+    @Override
     public Product update(Product updatedProduct) {
         for (int i = 0; i < productData.size(); i++) {
             if (productData.get(i).getProductId().equals(updatedProduct.getProductId())) {
@@ -43,4 +47,3 @@ public class ProductRepository {
         return null;
     }
 }
-
